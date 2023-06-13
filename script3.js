30b759f5d268149039683453889a6a9da203137a
MilanKlepar <135745127+MilanKlepar@users.noreply.github.com>
2023-06-13 10:51:28 +0200

    Manipulate the DOM with jQuery

--- script3.js
+++ script3.js
@@ -1,27 +1,32 @@
+$('body').css('background-image', 'url("./R.jpeg")');
 let names = ['Goga', 'Viki', 'Milan', 'Viktor', 'Patrik'];
 // console.log(names[1]);
 let li = $('li');
-// for (i = 1; i <= names.length; i++) {
+// for (i = 0; i <= names.length; i++) {
 //     li[i].append(names[i]);
 // }
 $('body').find('ul li');
 
 
 names.forEach ((i) => {
-
-    $('ul').append('<li class="added">', i ,'</li>');
-    // $('ul').children(".added").text(i);
-    $(i).css('background','purple');
+    
+    $('ul').append('<li class="add">',i,'</li>')
+    // for (l=0; l<= names.length; l++) {
+    //     $('li.add').text(i);
+    // }
+    // // m[i].append(names[i]);
+    // $(".added").append(i);
     
 })
-
-
-
-
-
- console.log(li.get(8))
-
-
+    console.log(li.get(8))
 $(li[2]).css('font-weight','bold');
 
-$('body').css('background-image', 'url("./R.jpeg")');
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+  $('ul').append('<h1 class="jsHEad"></h1>');
+  $('.jsHEad').text(additionalBlock.title);
+  $('.jsHEad').css('color','purple');
+  $('ul').append('<p class="jsP"></h1>');
+  $('.jsP').text(additionalBlock.text);
