ba27aa14f5ec4f4e4e47c24d8ab91f849dd07869
MilanKlepar <135745127+MilanKlepar@users.noreply.github.com>
2023-06-15 11:22:34 +0200

    Add some fun feature to your Event Listeners

--- script3.js
+++ script3.js
@@ -8,38 +8,48 @@ let li = $('li');
 $('body').find('ul li');
 
 
-names.forEach ((i) => {
-    
-    $('ul').append('<li class="add">',i,'</li>')
-    // for (l=0; l<= names.length; l++) {
-    //     $('li.add').text(i);
-    // }
-    // // m[i].append(names[i]);
-    // $(".added").append(i);
-    
+names.forEach((i) => {
+
+  $('ul').append('<li class="add">', i, '</li>')
+  // for (l=0; l<= names.length; l++) {
+  //     $('li.add').text(i);
+  // }
+  // // m[i].append(names[i]);
+  // $(".added").append(i);
+
 })
-    console.log(li.get(8))
-$(li[2]).css('font-weight','bold');
+// console.log(li.get(8))
+$(li[2]).css('font-weight', 'bold');
 
 let additionalBlock = {
-    title: "Added with javascript",
-    text: "This block was added using JavaScript's jQuery library. How awesome!"
-  };
-  $('ul').append('<h1 class="jsHEad"></h1>');
-  $('.jsHEad').text(additionalBlock.title);
-  $('.jsHEad').css('color','purple');
-  $('ul').append('<p class="jsP"></h1>');
-  $('.jsP').text(additionalBlock.text);
-  $('#fB').click(() => {
-    console.log("Yeah, you clicked me' to the console when the user clicks on the button");
-  })   
-  
-  $('#sB').click(() => {
-    $('#fB').text('Change text!Yupii');
-  })
-  $('#tB').click(() => {
-    $('button').css('background-color','gray');
-  })
- 
- 
+  title: "Added with javascript",
+  text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+$('ul').append('<h1 class="jsHEad"></h1>');
+$('.jsHEad').text(additionalBlock.title);
+$('.jsHEad').css('color', 'purple');
+$('ul').append('<p class="jsP"></h1>');
+$('.jsP').text(additionalBlock.text);
+$('#fB').click(() => {
+  console.log("Yeah, you clicked me' to the console when the user clicks on the button");
+})
+let p = $('#fB');
+let timesClicked = 0;
+$('#sB').click(() => {
+    p.toggle.text('Change text!Yupii');
+})
+let x = document.getElementById("txtIn");
+x.defaultValue = 'blue';
+if (x.defaultValue !== 'blue') {
+  x.value = InputEvent.onchange;
+}
+$('#tB').click(() => {
+  timesClicked++;
+  if (timesClicked <= 1){
+  $('button').css('background-color', x.value);
+}
+})
+
+console.log(x.value)
+// console.log(f);
 
