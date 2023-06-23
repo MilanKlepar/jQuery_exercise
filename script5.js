a23c91e18e9bc635bd58049e0b3a34c18802ea40
MilanKlepar <135745127+MilanKlepar@users.noreply.github.com>
2023-06-23 22:24:21 +0200

    Finally finished toDO list
    Sorry for so long time, i was just very busy this weeks

--- script5.js
+++ script5.js
@@ -1,4 +1,5 @@
 let addButton = $('#addButton');
+$('#addButton').html("<img src ='./add_library_my_icon1.png')>");
 let addIN = document.getElementById("addIN");
 // addIN.ariaPlaceholder();
 // addIN.defaultValue = 'write something';
@@ -13,16 +14,35 @@ addButton.click(() => {
     newLI.textContent = addIN.value;
     elements.append(newLI);
     ul.append(elements);
+    $(newLI).addClass('donePlan');
     $(newLI).attr('id','plan' + clickCount );
+   
     del = document.createElement('button');
     $(del).addClass('delbutton');
-    $(del).html('delete');
+    $(del).html('<img src="./delete_ic_icon.png">');
     newLI.append(del);
-    $(del).attr('id','del' + clickCount );
-    $(del).attr('data-plan', clickCount)
-    $(del).click (()=> {
-        $('#plan' + $(this.$(del)).attr('data-plan')).remove();
+    $(del).attr('data-plan', '#plan' + clickCount)
+    let planID = $(this.del).attr('data-plan');
     
+    done = document.createElement('button');
+    $(done).addClass('done');
+    $(done).html('<img src="./fluent_checkmark_circle_filled_icon.png">');
+    newLI.append(done);
+    $(done).attr ('id', 'done' + clickCount)
+    $(done).attr('data-done', '#done' + clickCount)
+    let doneID = $(this.done).attr('data-done');
+    
+    let cnt = 0;
+    $(done).click (()=>{
+        cnt++
+        $(planID).toggleClass ('donePlan');
+        if (cnt%2 == 1) {
+        $(doneID).html('<img src="./GREEN_fluent_checkmark_circle_filled_icon.png">');
+    } else $(doneID).html('<img src="./fluent_checkmark_circle_filled_icon.png">');
+    })
+
+    $(del).click (()=> {
+        $(planID).remove();
     })
     // $(del).fade(1000);
     // $(del)
